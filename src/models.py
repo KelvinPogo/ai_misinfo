936d6f0c85ebcde3cb93609b5c17459f83f6c5bb
Kevin Po <kevinpo>
2026-01-10 00:12:54 -0500

    Created folder structure, created models.py with ResNet and MobileNet imports, created training.py file

---
+++ src/models.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet50
+
+'''ResNet50'''
+resnet = resnet50(pretrained=True)
+resnet.fc = nn.Linear(resnet.fc.in_features, 2)
+
+'''mobilenetv3'''
+from torchvision.models import mobilenet_v3_large
+mobilenet = mobilenet_v3_large(pretrained=True)
+mobilenet.classifier[3] = nn.Linear(mobilenet.classifier[3].in_features, 2)
+
+
+
